ab7ff0db09f579021c269adc6b98f3c5b5235129
SRaswan <shauryaraswan@gmail.com>
2026-02-20 17:55:52 -0800

    lol

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use burn::backend::{Autodiff, NdArray};
+use burn::tensor::Tensor;
+
+type Backend = Autodiff<NdArray>;
+type Device = <NdArray as burn::tensor::backend::Backend>::Device;
+
+fn main() {
+    let device = Device::default();
+
+    // x = 2.0, compute y = x^2 and get dy/dx
+    let x: Tensor<Backend, 1> = Tensor::from_floats([2.0_f32], &device).require_grad();
+    let y = x.clone() * x.clone();
+
+    let grads = y.backward();
+    let x_grad = x.grad(&grads).unwrap();
+
+    println!("y = x^2,  x = 2.0");
+    println!("dy/dx = {:?}", x_grad.into_data()); // expected: 4.0
+}
